39e64add33433d2008226a1cfdc10075c6a200c2
Nicolas Matheus Ferreira <71523671+NicolasMatheusFerreira@users.noreply.github.com>
2023-10-28 07:50:58 -0300

    Iniciando projeto

---
+++ back/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace back {
+    class Program {
+        static void Main(string[] args) {
+            Candidato candidato = new Candidato();
+            candidato.NomeCompleto = "Luis Inacio Da Silva";
+
+            candidato.apresentaCandidato();
+        }
+    }
+}
